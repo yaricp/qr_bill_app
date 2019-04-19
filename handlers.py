e4aaa2793eb873ffe1b55671267caa80fbf16613
yaricp <yaricp@gmail.com>
2019-04-20 01:21:02 +0700

    float

--- handlers.py
+++ handlers.py
@@ -91,6 +91,7 @@ def new_msg(bot, update):
         result_text = ''
         for rec in list_decoded:
             list_data = rec.data.decode("utf-8").split('&')
+            print(list_data[1])
             date_time = datetime.strptime(list_data[0].replace('t=', ''), '%Y%m%dT%H%M%S').date()
             summ = float(list_data[1].replace('s=', ''))
             type_data = rec.type
