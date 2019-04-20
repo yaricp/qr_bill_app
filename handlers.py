c1519437369d7c412da2716a741599b648f2d020
yaricp <yaricp@gmail.com>
2019-04-20 19:45:12 +0700

    views.py

--- handlers.py
+++ handlers.py
@@ -11,6 +11,7 @@ from PIL import Image
 
 from config import *
 from db_models import *
+from views import *
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -116,14 +117,10 @@ def list_purchase(bot, update):
 
                                 
 def menu(bot, update):
-    buttons = [[InlineKeyboardButton( 'new_category', callback_data='/new_category'), 
-                InlineKeyboardButton( 'new_seller', callback_data='/new_seller')],  
-                [InlineKeyboardButton( 'list', callback_data='/list')], 
-                [InlineKeyboardButton( 'orders', callback_data='/orders')], 
-                ]
-    keyboard = InlineKeyboardMarkup(buttons)
-    update.message.reply_text(  text='Menu',
-                                reply_markup=keyboard)
+    show_menu(bot, update.message)
+    
+    
+
     
     
 def error(bot, update, error_msg):
@@ -135,12 +132,7 @@ def list_orders(bot, update):
     show_orders(bot, update.message)
     
     
-def show_orders(bot, message):
-    buttons = [[InlineKeyboardButton( 'by_category', callback_data='/by_category'), 
-                InlineKeyboardButton( 'by_seller', callback_data='/by_seller')]]
-    keyboard = InlineKeyboardMarkup(buttons)
-    message.reply_text( text='Orders',
-                        reply_markup=keyboard)
+
 
                         
 @is_allowed_user()
@@ -152,25 +144,6 @@ def by_sellers(bot, update):
 def by_categories(bot, update):
     show_order_by(bot, 'category', update.message)
     
-    
-def show_order_by(bot, type, message):
-    keyboard = get_main_button()
-    text = ''
-    if type == 'seller':
-        sellers = Seller.select()
-        for s in sellers:
-            summ = Purchase.select(fn.SUM(Purchase.summ)).where(Purchase.seller == s).scalar()
-            text += 'Seller: %s, Summa: %s\n' % (s.name,  summ)
-    else:
-        categories = Categories.select()
-        for c in categories:
-            summ = Purchase.select(fn.SUM(Purchase.summ)).where(Purchase.category == c).scalar()
-            text += 'Categories: %s, Summa: %s\n' % (c.name, summ)
-    bot.send_message(message.chat.id,
-                    text=text,
-                    reply_markup=keyboard
-                    )
-
     
 @is_allowed_user()
 def new_category(bot, update):
@@ -270,26 +243,6 @@ def new_msg(bot, update):
             update.message.reply_text(text='Category created!')
         
 
-def show_purchase_item(bot, message, id):
-    purchase = Purchase.get(Purchase.id==id)
-    category_name = ''
-    seller_name = ''
-    keyboard = get_button_categories(id)
-    if purchase.category:
-        category_name = purchase.category.name
-    if purchase.seller:
-        seller_name = purchase.seller.name
-    text = 'ID: %s\nDate Time: %s\nSumma: %s\nSeller: %s\nCategory: %s' % ( 
-            purchase.id, 
-            purchase.datetime, 
-            purchase.summ, 
-            seller_name, 
-            category_name
-                            )
-    bot.send_message(message.chat.id,
-                    text=text,
-                    reply_markup=keyboard
-                    )
 
 
 def button(bot, update):
