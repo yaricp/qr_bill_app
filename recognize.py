ad4661725bbcfcc3ca015537aa00c7454fd3295d
yaricp <yaricp@gmail.com>
2019-04-22 02:16:39 +0700

    pytesseract

--- recognize.py
+++ recognize.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from pyzbar.pyzbar import decode
 from PIL import Image
+import pytesseract
 
 from config import *
 
@@ -10,14 +11,19 @@ def scan():
     summ = None
     list_decoded = decode(Image.open(os.path.join(PATH_TEMP_FILES,'qrcode.jpg')))
     print('LIST_DECODED: ', list_decoded)
-    for rec in list_decoded:
-        print('TYPE_CODE: ', rec.type)
-        type_data = rec.type
-        if type_data == 'QRCODE':
-            list_data = rec.data.decode("utf-8").split('&')
-            date_time = datetime.strptime(list_data[0].replace('t=', ''), '%Y%m%dT%H%M%S').date()
-            summ = float(list_data[1].replace('s=', ''))
-        elif type_data == 'EAN13':
-            list_data = rec.data.decode("utf-8")
-            print(list_data)
+    if list_decoded:
+        for rec in list_decoded:
+            print('TYPE_CODE: ', rec.type)
+            type_data = rec.type
+            if type_data == 'QRCODE':
+                list_data = rec.data.decode("utf-8").split('&')
+                date_time = datetime.strptime(list_data[0].replace('t=', ''), '%Y%m%dT%H%M%S').date()
+                summ = float(list_data[1].replace('s=', ''))
+            elif type_data == 'EAN13':
+                list_data = rec.data.decode("utf-8")
+                print(list_data)
+    else:
+        raw_text = pytesseract.image_to_string(Image.open(os.path.join(PATH_TEMP_FILES,'qrcode.jpg')),
+                                                lang=LANG)
+        print('RAW_TEXT: ', raw_text)
     return date_time, summ
