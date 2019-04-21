0c6e3e9e701bb2771ede466e055ed96af6bff8a2
yaricp <yaricp@gmail.com>
2019-04-22 00:14:40 +0700

    NoneTYpe

--- keyboards.py
+++ keyboards.py
@@ -93,6 +93,7 @@ def get_button_list_purchase():
     buttons = []
     seller_name = 'None'
     for p in purchases:
+        print('DICT: ', p.__dict__)
         if not p.seller:
             seller_name = 'None'
         else:
