2dd13de4202cad7f65397e3ccda1445092d9fbc9
yaricp <yaricp@gmail.com>
2019-06-05 17:14:28 +0700

    del unused field

--- migrations/004_rename_category_in_seller.py
+++ migrations/004_rename_category_in_seller.py
@@ -5,9 +5,8 @@ from playhouse.migrate import *
 sys.path.append(os.getcwd())
 
 from config import *
-import peewee as pw
+#import peewee as pw
 
-category = pw.ForeignKeyField(Category, field=Category.id, backref='categories', null=True)
 migrate(
     migrator.rename_column('Seller', 'category', 'category_id'),
 )
