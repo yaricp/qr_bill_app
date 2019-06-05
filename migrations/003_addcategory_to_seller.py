01df28fe6a2a4e483f76ce33165a290c801725ed
yaricp <yaricp@gmail.com>
2019-06-05 16:20:12 +0700

    added category for seller

--- migrations/003_addcategory_to_seller.py
+++ migrations/003_addcategory_to_seller.py
@@ -7,8 +7,8 @@ sys.path.append(os.getcwd())
 from config import *
 import peewee as pw
 
-cat_field = pw.ForeignKeyField('Category', backref='categories', null=True)
+category = pw.ForeignKeyField(Category, backref='categories', null=True)
 migrate(
-    migrator.add_column('Seller', 'category', cat_field),
+    migrator.add_column('Seller', 'category', category),
     
 )
