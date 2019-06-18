60f39ee0302f22f628140be722764786a1e35630
yaricp <yaricp@gmail.com>
2019-06-18 16:00:04 +0700

    translate

--- prepare_po_file.sh
+++ prepare_po_file.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#if [[ "$OSTYPE" == "linux-gnu" ]]; then
+#        GETTEXT_CMD="gettext"
+#elif [[ "$OSTYPE" == "darwin"* ]]; then
+#        GETTEXT_CMD="xgettext"
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+#        GETTEXT_CMD="gettext"
+#elif [[ "$OSTYPE" == "msys" ]]; then
+#        GETTEXT_CMD="gettext"
+#elif [[ "$OSTYPE" == "win32" ]]; then
+#        GETTEXT_CMD="gettext"
+#elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#        GETTEXT_CMD="gettext"
+#else
+GETTEXT_CMD="xgettext"
+#fi
+
 work_dir=$PWD
 LANG_DIR="lang/ru/LC_MESSAGES"
 args=("$@")
@@ -7,6 +23,7 @@ echo $args
 file_name=${args##*/}
 file_name=${file_name//'.py'/''}
 #echo $file_name
-xgettext -o $LANG_DIR/prepare_$file_name.pot $args
+
+$GETTEXT_CMD -o $LANG_DIR/prepare_$file_name.pot $args
 #echo " PATH  - $LANG_DIRprepare_$file_name.pot"
 msgmerge -U --no-fuzzy-matching --backup=off $LANG_DIR/messages.po $LANG_DIR/prepare_$file_name.pot
