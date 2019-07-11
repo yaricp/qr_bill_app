3d7cbfdf8f5167b5eca9523768457182256cdc2e
yaricp <yaricp@gmail.com>
2019-07-11 20:04:43 +0700

    adedd orders by month

--- views.py
+++ views.py
@@ -29,12 +29,16 @@ def show_order_by(user, type):
         categories = Category.select().where(Category.user==user)
         #text += _('Month: ')+ str(month + 1)+ '\n'
         for c in categories:
-            summ = (Purchase.select(fn.SUM(Purchase.summ))
+            try:
+                summ = (Purchase.select(fn.SUM(Purchase.summ))
                 .where(Purchase.category == c)
                 .where(fn.date_part('year', Purchase.datetime) == 2019)
                 .group_by(Purchase.datetime, month)
                 .order_by(Purchase.datetime, month)
                 .scalar())
+            except: 
+                summ=0 
+                print('error')
             print('summ :', summ)
             text += _('Category: %(cat)s, Summa: %(summ)s\n') % ({'cat':c.name, 'summ':summ})
         categories = Category.select().where(Category.user==user)
