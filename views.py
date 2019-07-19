32ba6cd0d0c0f53a213d03b146c11ce3f2886e4a
yaricp <yaricp@MacUsers-Mac-mini.local>
2019-07-19 21:22:38 +0700

    format table foe html

--- views.py
+++ views.py
@@ -55,6 +55,7 @@ def show_order_by(user, type):
     
     for m in (month_now-2, month_now-1, month_now):
         count_m += 1
+        print('m: ', m)
         dict_column_size.update({count_m:len(dict_months[m])})
         print(type(len(dict_months[m])))
         total_size += len(dict_months[m])
