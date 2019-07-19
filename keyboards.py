bb117b1724680bfff6ddb2c8abdd3eda2e5691e3
yaricp <yaricp@MacUsers-Mac-mini.local>
2019-07-20 00:18:01 +0700

    format table foe html

--- keyboards.py
+++ keyboards.py
@@ -285,7 +285,6 @@ def get_button_confirm(id):
 
 def get_button_order_by(user, type_c):
     month_now = datetime.now().month
-    keyboard = []
     if type_c == 'seller':
         list_by_for = Seller.select().where(Seller.user==user)
         by_for_field = Purchase.seller
@@ -294,13 +293,13 @@ def get_button_order_by(user, type_c):
         list_by_for = Category.select().where(Category.user==user)
         by_for_field = Purchase.category
         c_name = 'Category'
-        
+    buttons = []    
     for c in list_by_for:
-        buttons = []
+        rows = []
         c_name = '-'
         if c.name:
             c_name = c.name
-        buttons.append(InlineKeyboardButton( c_name, callback_data='/by_category&%s' % c_name ))
+        rows.append(InlineKeyboardButton( c_name, callback_data='/by_category&%s' % c_name ))
         for m in (month_now-2, month_now-1, month_now):
             month = str(m)
             if m < 10:
@@ -312,6 +311,7 @@ def get_button_order_by(user, type_c):
                 )
             if summ: summ = str(round(summ, 2))
             else: summ = ''
-            buttons.append(InlineKeyboardButton( summ, callback_data='/by_category&%s&%s' % (c_name, m) ))
-        keyboard.append(buttons)
+            rows.append(InlineKeyboardButton( summ, callback_data='/by_category&%s&%s' % (c_name, m) ))
+        buttons.append(rows)
+    keyboard = InlineKeyboardMarkup(buttons)
     return keyboard
