170996315a906d85762f962774e373f4566da8e8
yaricp <yaricp@gmail.com>
2019-09-06 17:27:04 +0700

    locations

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ def main():
 #        allow_reentry = True
 #    )
 #    dispatcher.add_handler(new_photo_handler)
-    location = MessageHandler(Filters.location, location, edited_updates=True)
-    dispatcher.add_handler(location)
+    location_handler = MessageHandler(Filters.location, location, edited_updates=True)
+    dispatcher.add_handler(location_handler)
     
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
