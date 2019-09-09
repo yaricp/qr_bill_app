4e460e2e14debc7913e122dc3312c485fbe6aa89
yaricp <yaricp@gmail.com>
2019-09-09 15:51:46 +0700

    test search seller by geo

--- keyboards.py
+++ keyboards.py
@@ -108,9 +108,9 @@ def get_button_sellers(user, id_item, geo=None):
 
     curs.execute("""\
         SELECT seller.id,seller.name,
-        ST_Distance((seller.longitude,seller.latitude), poi)
+        ST_Distance((seller.longitude::double precision,seller.latitude::double precision), poi)
         FROM seller, (SELECT ST_MakePoint(%s, %s)::geography AS poi) AS f
-        WHERE ST_DWithin((longitude,latitude), poi, 1000);""", poi)
+        WHERE ST_DWithin((seller.longitude::double precision,seller.latitude::double precision), poi, 1000);""", poi)
     print('result of geo search: ',curs.fetchall())
     for row in curs.fetchall():
         print(row)
