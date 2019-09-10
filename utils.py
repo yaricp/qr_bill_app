9c8c5f4f8c8bffc18e4b8240c71e7a425056ef31
yaricp <yaricp@gmail.com>
2019-09-10 14:37:45 +0700

    show venue

--- utils.py
+++ utils.py
@@ -31,10 +31,9 @@ def get_geo_positions(type_obj, id):
     geo = None
     sql_text = 'SELECT ST_AsText(geom) FROM %s WHERE id=%s' % (type_obj, id)
     curs.execute(sql_text)
-    result = curs.fetchall()[0] 
-    point = result[0].replace('POINT(', '').replace(')', '').split(' ')
-    print('TYpe POINT:', type(point))
+    result = curs.fetchall()[0]
     if point:
+        point = result[0].replace('POINT(', '').replace(')', '').split(' ')
         geo = (point[0], point[1])
     conn.close()
     return geo 
