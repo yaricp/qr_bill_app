9dbea775818ebdacf5ae0d1084454dddec8f6c4d
yaricp <yaricp@gmail.com>
2019-09-10 14:09:01 +0700

    show venue

--- handlers.py
+++ handlers.py
@@ -274,12 +274,16 @@ def set_location(bot, update):
                     chat_id=chat_id,
                     message_id=message_id
                     )
-    bot.send_message(
+    bot.delete_message(
                     chat_id=chat_id,
-                    reply_markup=ReplyKeyboardRemove())
+                    message_id=message_id-1
+                    )
+#    bot.send_message(
+#                    chat_id=chat_id,
+#                    reply_markup=ReplyKeyboardRemove())
     bot.edit_message_text(
                         chat_id=chat_id,
-                        message_id=message_id-1, 
+                        message_id=message_id-2, 
                         text=text, 
                         reply_markup=keyboard)
 
