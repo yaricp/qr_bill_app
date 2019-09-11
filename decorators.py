8c49ddea935b63653150a5707b0cc84647df6022
yaricp <yaricp@gmail.com>
2019-09-11 16:41:22 +0700

    decorators

--- decorators.py
+++ decorators.py
@@ -8,18 +8,18 @@ def is_allowed_user():
     def wrap(f):
         def wrapped_f(*args, **kwargs):
             obj = None
-            if args[1].message:
-                obj = args[1].message
-            elif args[1].callback_query:
-                obj = args[1].callback_query
+            if args[0].message:
+                obj = args[0].message
+            elif args[0].callback_query:
+                obj = args[0].callback_query
             if obj:
                 user_id = obj.from_user.id
                 user = User.get_or_none(User.tg_user_id==user_id)
                 if user and user.is_active:
                     return f(*args, **kwargs)
                 else:
-                    if args[1].callback_query:
-                        args[1].callback_query.answer('Sorry! you need to register. Use /start for this.')
+                    if args[0].callback_query:
+                        args[0].callback_query.answer('Sorry! you need to register. Use /start for this.')
                     else:
                         obj.reply_text('Sorry! you need to register. Use /start for this.')
         return wrapped_f
@@ -30,16 +30,16 @@ def is_admin():
     def wrap(f):
         def wrapped_f(*args, **kwargs):
             obj = None
-            if args[1].message:
-                obj = args[1].message
-            elif args[1].callback_query:
-                obj = args[1].callback_query
+            if args[0].message:
+                obj = args[0].message
+            elif args[0].callback_query:
+                obj = args[0].callback_query
             if obj:
                 if obj.from_user.first_name in admins:
                     f(*args, **kwargs)
                 else:
-                    if args[1].callback_query:
-                        args[1].callback_query.answer('Sorry! you are not admin...')
+                    if args[0].callback_query:
+                        args[0].callback_query.answer('Sorry! you are not admin...')
                     else:
                         obj.reply_text('Sorry! you are not admin...')
         return wrapped_f
@@ -52,10 +52,10 @@ def is_not_bot():
             obj = None
             print('args[1]: ', args[1].__dict__)
             print('args[0]: ', args[0].__dict__)
-            if args[1].message:
-                obj = args[1].message
-            elif args[1].callback_query:
-                obj = args[1].callback_query
+            if args[0].message:
+                obj = args[0].message
+            elif args[0].callback_query:
+                obj = args[0].callback_query
             if obj:
                 if not obj.from_user.is_bot:
                     return f(*args, **kwargs)
@@ -67,10 +67,10 @@ def lang():
     def wrap(f):
         def wrapped_f(*args, **kwargs):
             obj = None
-            if args[1].message:
-                obj = args[1].message
-            elif args[1].callback_query:
-                obj = args[1].callback_query
+            if args[0].message:
+                obj = args[0].message
+            elif args[0].callback_query:
+                obj = args[0].callback_query
             if obj:
                 user = obj.from_user.id
                 lang = obj.from_user.language_code
