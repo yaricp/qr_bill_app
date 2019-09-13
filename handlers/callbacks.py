037fafebdc8d38d7fd8afae0ff13c76538982fe4
yaricp <yaricp@MacUsers-Mac-mini.local>
2019-09-13 15:39:12 +0700

    new_category new_seller

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -345,9 +345,10 @@ def change_seller_category_purchase(update, context):
         obj = change_category(obj, user, id_link_obj, type_obj)
     if type_obj == 'purchase':
         text = show_purchase_item(user, obj.id)
+        keyboard = buttons_for_purchase_item(user, obj.id)
     else:
         text = show_seller_item(user, obj.id)
-    keyboard = buttons_for_purchase_item(user, obj.id)
+        keyboard = buttons_for_seller_item(user, obj.id)
     context.user_data.clear()
     context.bot.edit_message_text(
         chat_id=chat_id, 
