ec610ac9b7cc2edcbcd73552c48ef0efbaf03aed
yaricp <yaricp@gmail.com>
2019-09-17 17:27:44 +0700

    search by radius

--- keyboards.py
+++ keyboards.py
@@ -166,9 +166,10 @@ def get_button_sellers(user, id_item, geo=None, radius=RADIUS_SEARCH_SELLER):
        
     menu.append(buttons)
     if seller_dict_flag:
+        new_radius = int(radius)*2
         new_button = InlineKeyboardButton(  
-            _('Search %s around' % (radius*2)),
-            callback_data='search_by_radius&%s' % (radius*2))
+            _('Search %s around' % new_radius),
+            callback_data='search_by_radius&%s' % new_radius)
         menu.append([new_button])
         new_button = InlineKeyboardButton(  
             _('Show all sellers'),
