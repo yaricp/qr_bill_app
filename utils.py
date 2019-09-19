c7d58de8c9f136babb586b260a61231a4ad7f2fa
yaricp <yaricp@MacUsers-Mac-mini.local>
2019-09-19 23:41:18 +0700

    AND

--- utils.py
+++ utils.py
@@ -86,9 +86,9 @@ def find_sellers_around(user, geo, radius_mi=RADIUS_SEARCH_SELLER):
     radius = int(radius_mi)/1000
     curs.execute(
         'SELECT id,name FROM seller '\
-        'WHERE user=%s AND ST_Distance_Sphere(geom, ST_SetSRID(ST_MakePoint('\
+        'WHERE user=\'%s\' AND ST_Distance_Sphere(geom, ST_SetSRID(ST_MakePoint('\
         '%s, %s), 4326)) <= %s * 1609.34;', 
-        (int(user), geo.longitude, geo.latitude, radius))
+        (user, geo.longitude, geo.latitude, radius))
     if DEVEL: print('found: ', curs)
     
     return curs.fetchall()
