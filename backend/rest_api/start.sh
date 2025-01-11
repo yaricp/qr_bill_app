1939b9e19b3e3ff8cc2ce066491e54226f2fed47
Yaroslav Pisarev <yaricp@gmail.com>
2025-01-11 23:11:41 +0100

    prepare test-action

--- backend/rest_api/start.sh
+++ backend/rest_api/start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# echo "Prepare GRPC server"
+echo "Prepare GRPC server"
 
-# bash /scripts/prepare_grpc_proto.sh
+bash /scripts/prepare_grpc_proto.sh
 
-# echo "change /src/infra/grpc_server/grpc_pb2_grpc.py"
-# echo "  from: 'import grpc_pb2 as grpc__pb2'"
-# echo " to: 'from . import grpc_pb2 as grpc__pb2'"
-# cp /src/infra/grpc_server/grpc_pb2_grpc.py /src/infra/grpc_server/grpc_pb2_grpc.py.orig
-# sed 's/import grpc_pb2/from . import grpc_pb2/g' /src/infra/grpc_server/grpc_pb2_grpc.py.orig > /src/infra/grpc_server/grpc_pb2_grpc.py
+echo "change /src/infra/grpc_server/grpc_pb2_grpc.py"
+echo "  from: 'import grpc_pb2 as grpc__pb2'"
+echo " to: 'from . import grpc_pb2 as grpc__pb2'"
+cp /src/infra/grpc_server/grpc_pb2_grpc.py /src/infra/grpc_server/grpc_pb2_grpc.py.orig
+sed 's/import grpc_pb2/from . import grpc_pb2/g' /src/infra/grpc_server/grpc_pb2_grpc.py.orig > /src/infra/grpc_server/grpc_pb2_grpc.py
 
 echo "Start GRPC Server"
 
