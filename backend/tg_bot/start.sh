9b6765835045693c0eb683692b5163fd0fc22754
Yaroslav Pisarev <yaricp@gmail.com>
2025-01-11 23:41:58 +0100

    prepare test-action

--- backend/tg_bot/start.sh
+++ backend/tg_bot/start.sh
@@ -4,11 +4,11 @@ echo "Prepare GRPC server"
 
 bash /scripts/prepare_grpc_proto.sh
 
-echo "change /bot/app/grpc_client/grpc_pb2_grpc.py"
+echo "change /bot/grpc_client/grpc_pb2_grpc.py"
 echo "  from: 'import grpc_pb2 as grpc__pb2'"
 echo " to: 'from . import grpc_pb2 as grpc__pb2'"
-cp /bot/app/grpc_client/grpc_pb2_grpc.py /bot/app/grpc_client/grpc_pb2_grpc.py.orig
-sed 's/import grpc_pb2/from . import grpc_pb2/g' /bot/app/grpc_client/grpc_pb2_grpc.py.orig > /bot/app/grpc_client/grpc_pb2_grpc.py
+cp /bot/grpc_client/grpc_pb2_grpc.py /bot/grpc_client/grpc_pb2_grpc.py.orig
+sed 's/import grpc_pb2/from . import grpc_pb2/g' /bot/grpc_client/grpc_pb2_grpc.py.orig > /bot/grpc_client/grpc_pb2_grpc.py
 
 echo "Start main TG bot"
 
