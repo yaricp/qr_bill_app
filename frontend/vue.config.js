67d188a38949fc32a7471cc95f0ba600867dc762
Yaroslav Pisarev <yaricp@gmail.com>
2025-01-12 23:04:06 +0100

    added PWA to Vue 3 app

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,20 +8,10 @@ module.exports = {
     port: 8081
   },
   pwa: {
-    name: 'My ToDo App',
-    short_name: 'ToDo',
-    description: 'A ToDo progressive web app',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#41b383',
-    icons: [
-      {
-        src: '/img/icons/icon-72x72.png',
-        sizes: '72x72',
-        type: 'image/png',
-      },
-      // Add more icons for different resolutions as needed
-    ],
-  },
+    workboxPluginMode: 'GenerateSW',
+    manifestOptions: {
+      name: "MyApp",
+      short_name: "MyApp",
+    }
+  }
 }
