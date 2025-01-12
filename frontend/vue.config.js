86ef50d3580b4077a422af7e00fba60307034fbc
Yaroslav Pisarev <yaricp@gmail.com>
2025-01-12 23:14:08 +0100

    added PWA to Vue 3 app

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -12,6 +12,10 @@ module.exports = {
     manifestOptions: {
       name: "MyApp",
       short_name: "MyApp",
+      icons: [
+        {"src":"./img/icons/qracun.png","sizes":"512x512","type":"image/png"}
+      ],
+      background_color: "#12b0db"
     }
   }
 }
