e38e8b698cd26683916358f5827abdb36dcf2a9e
Yaroslav Pisarev <yaricp@gmail.com>
2025-08-24 12:26:32 +0200

    a new system push images and defining a name images for helm

--- backend/rest_api/start.sh
+++ backend/rest_api/start.sh
@@ -10,20 +10,13 @@ echo " to: 'from . import grpc_pb2 as grpc__pb2'"
 cp /src/infra/grpc_server/grpc_pb2_grpc.py /src/infra/grpc_server/grpc_pb2_grpc.py.orig
 sed 's/import grpc_pb2/from . import grpc_pb2/g' /src/infra/grpc_server/grpc_pb2_grpc.py.orig > /src/infra/grpc_server/grpc_pb2_grpc.py
 
-echo "Start GRPC Server"
-
-poetry run python start_grpc_server.py &
-
 echo "Migrations"
 
 poetry run alembic revision --autogenerate -m 'init'
 poetry run alembic upgrade head
 
-# echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-
-# product start
-# echo "scripts prepare"
-# scripts/prepare_data_in_db.sh
+echo "Start GRPC Server"
+poetry run python start_grpc_server.py &
 
 echo "DEBUG: $DEBUG";
 if test "$DEBUG" = "true" || test "$DEBUG" = "True"
