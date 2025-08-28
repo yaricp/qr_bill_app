622febded58f99e3a07c730ea4940c1bbce7b20f
Yaroslav Pisarev <yaricp@gmail.com>
2025-08-29 00:11:55 +0200

    fixed settings serviceMonitor for api

--- backend/rest_api/src/api/v1/endpoints/metrics.py
+++ backend/rest_api/src/api/v1/endpoints/metrics.py
@@ -4,10 +4,11 @@ from fastapi.responses import Response
 from ... import app
 
 
-@app.get("/metrics")
+@app.get("/metrics", tags=['Bills'])
 def metrics():
     """
-    Endpoint, which is used by Prometheus to scrape the metrics."""
+    Endpoint, which is used by Prometheus to scrape the metrics.
+    """
     return Response(
         generate_latest(), media_type=CONTENT_TYPE_LATEST
     )
