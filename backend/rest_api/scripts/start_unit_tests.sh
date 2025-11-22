2980e1e28b172e70b431f04ceafe4dc33e8255d6
Yaroslav Pisarev <yaricp@gmail.com>
2025-11-22 23:39:04 +0100

    app unit tests

--- backend/rest_api/scripts/start_unit_tests.sh
+++ backend/rest_api/scripts/start_unit_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-env PYTHONPATH=$PYTHONPATH:/src poetry run pytest /src/tests/app/test_category.py;
+env PYTHONPATH=$PYTHONPATH:/src poetry run pytest /src/tests/api/smoke_tests;
 
 # echo "Starting test_category.py";
 # env PYTHONPATH=$PYTHONPATH:/src poetry run pytest /src/tests/app/test_category.py;
